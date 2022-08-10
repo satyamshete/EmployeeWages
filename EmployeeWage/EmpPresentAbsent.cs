f9a6f18c6a9c095dce05ebb64786f48474025b78
satyamshete <shetesatyam99@gmail.com>
2022-08-11 00:26:37 +0530

    [Satyam] Add: UC1 employee present absent

---
+++ EmployeeWage/EmpPresentAbsent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmpPresentAbsent
+    {
+        public void EmployeepresentAbsent()
+        {
+            Console.WriteLine("UC1-Employee present or absent");
+            Random rand = new Random();
+            int empCheck = rand.Next(1, 3); // it will give values from 1 till 3 but not 3 so possible output is 1,2
+            int isFullTime = 1;
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program !!!");
+
+            ////UC1
+            EmpPresentAbsent empPresentAbsent = new EmpPresentAbsent();
+            empPresentAbsent.EmployeepresentAbsent();
+           
+            Console.WriteLine();
+        }
+    }
+}
